fccf4cd49b6e353ab2a9e779022e37875acf241e
@shmulim <shmuel.markel@gmail.com>
2015-06-18 11:05:19 -0700

    Update sum-and-multiply.js

--- js/sum-and-multiply.js
+++ js/sum-and-multiply.js
@@ -1,17 +1,15 @@
 array = [1, 2, 3, 4, 5];
 
 function sum(array) {
-    var arr = array.reduce(function(x, y) {
+    return array.reduce(function(x, y) {
         return x + y;
     });
-    return arr;
 }
 
 function multiply(array) {
-    var arr = array.reduce(function(x, y) {
+    return array.reduce(function(x, y) {
         return x * y;
     });
-    return arr;
 }
 
 sum(array); // 15
